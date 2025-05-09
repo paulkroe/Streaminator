b5ac08223158e73da12d347e8717f498043f99af
Paul Kroeger <paul.kroeger@columbia.edu>
2025-05-08 21:34:31 -0400

    Fixed command acceptance_rates_plots.sh

--- acceptance_rates_plots.sh
+++ acceptance_rates_plots.sh
@@ -1,3 +1,3 @@
-!python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2 --no_prompt_training
-!python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2 --no_generation_training
-!python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2
+python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2 --no_prompt_training
+python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2 --no_generation_training
+python main.py --num_completions 8 --num_samples 128 --max_length 250 --stream_width 64 --use_wandb --ngram_order 2
